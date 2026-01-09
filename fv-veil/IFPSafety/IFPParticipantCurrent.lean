b925aab4f8f945acf0e19a7eccab24a8f1f56c75
Rishal SP <rishal@sarva.ai>
2026-01-09 22:50:45 +0530

    inv fix

--- fv-veil/IFPSafety/IFPParticipantCurrent.lean
+++ fv-veil/IFPSafety/IFPParticipantCurrent.lean
@@ -883,7 +883,7 @@ invariant [precommit_only_if_prepare]
   ((cur_stage N V p1_fixed p2_fixed precommit ∨ precommitted_node N V p1_fixed p2_fixed I) ∧ I ≠ genesis) →  prepared_node N V p1_fixed p2_fixed
 
 invariant [precommit_lock_stage]
-  (locked N p1_fixed I false V ∨ locked N p2_fixed I false V) → (cur_stage N V p1_fixed p2_fixed precommit ∨ cur_stage N V p1_fixed p2_fixed commit)
+  ((locked N p1_fixed I false V ∨ locked N p2_fixed I false V) ∧ I ≠ genesis) → (cur_stage N V p1_fixed p2_fixed precommit ∨ cur_stage N V p1_fixed p2_fixed commit)
 
 invariant [cur_stage_exists]
   cur_stage N V p1_fixed p2_fixed prepare ∨ cur_stage N V p1_fixed p2_fixed propose ∨ cur_stage N V p1_fixed p2_fixed prevote \
