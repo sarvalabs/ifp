52c09d221a143c23ea94034423181604d2887734
Rishal SP <rishal@sarva.ai>
2026-01-21 16:36:43 +0530

    inv fix - del forall in conclusion

--- fv-veil/IFPSafety/IFPParticipantCurrent.lean
+++ fv-veil/IFPSafety/IFPParticipantCurrent.lean
@@ -839,14 +839,14 @@ invariant [precommit_next_view_discovery]
      tot_view.lt v v2 ∧ -- Enforce v < v2 (next should imply this)
      ¬ tot_view.lt v2 v ∧ -- No cycles
      ¬ tot_view.lt v2 tot_view.zero ∧ -- v2 cannot be less than zero
-     ((¬ is_byz n ∧ ctx.member n c1 ∨ ctx.member n c2) →
+     ((¬ is_byz n ∧ (ctx.member n c1 ∨ ctx.member n c2)) →
       ((ctx.member n c1 → locked n p1_fixed i precommit v ) ∧
        (ctx.member n c2 → locked n p2_fixed i precommit v) ∧
        cur_stage n v p1_fixed p2_fixed commit ∧
        prepared_node n v2 p1_fixed p2_fixed ∧
        cur_view n v2
-      ))) →
-    (∀ (n : node), ¬ is_byz n → (
+      )) ) →
+    (¬ is_byz n → (
       (ctx.member n c1 → (sent_lock_in_prepare_1 n v2 p1_fixed p2_fixed i precommit v)) ∧
       (ctx.member n c2 → (sent_lock_in_prepare_2 n v2 p1_fixed p2_fixed i precommit v)))
     )
@@ -868,14 +868,14 @@ invariant [precommit_next_view_discovery_sup]
      tot_view.lt v v2 ∧ -- Enforce v < v2 (next should imply this)
      ¬ tot_view.lt v2 v ∧ -- No cycles
      ¬ tot_view.lt v2 tot_view.zero ∧ -- v2 cannot be less than zero
-     ¬ is_byz n →
+     ((¬ is_byz n ∧ (ctx.member n s1 ∨ ctx.member n s2)) →
       ((ctx.member n s1 → locked n p1_fixed i precommit v) ∧
        (ctx.member n s2 → locked n p2_fixed i precommit v) ∧
        cur_stage n v p1_fixed p2_fixed commit ∧
        prepared_node n v2 p1_fixed p2_fixed ∧
        cur_view n v2
-      )) →
-    (∀ (n : node), ¬ is_byz n → (
+      )) ) →
+    (¬ is_byz n → (
       (ctx.member n s1 → (sent_lock_in_prepare_1 n v2 p1_fixed p2_fixed i precommit v)) ∧
       (ctx.member n s2 → (sent_lock_in_prepare_2 n v2 p1_fixed p2_fixed i precommit v)))
     )
@@ -897,14 +897,14 @@ invariant [prevote_next_view_discovery_sup]
      tot_view.lt v v2 ∧ -- Enforce v < v2 (next should imply this)
      ¬ tot_view.lt v2 v ∧ -- No cycles
      ¬ tot_view.lt v2 tot_view.zero ∧ -- v2 cannot be less than zero
-     ¬ is_byz n →
+     ((¬ is_byz n ∧ (ctx.member n s1 ∨ ctx.member n s2)) →
       ((ctx.member n s1 → (locked n p1_fixed i prevote v ∧ ¬ locked n p1_fixed i precommit v) ) ∧
        (ctx.member n s2 → (locked n p2_fixed i prevote v ∧ ¬ locked n p2_fixed i precommit v) ) ∧
        cur_stage n v p1_fixed p2_fixed commit ∧
        prepared_node n v2 p1_fixed p2_fixed ∧
        cur_view n v2
-      )) →
-    (∀ (n : node), ¬ is_byz n → (
+      )) ) →
+    (¬ is_byz n → (
       (ctx.member n s1 → (sent_lock_in_prepare_1 n v2 p1_fixed p2_fixed i prevote v)) ∧
       (ctx.member n s2 → (sent_lock_in_prepare_2 n v2 p1_fixed p2_fixed i prevote v)))
     )
