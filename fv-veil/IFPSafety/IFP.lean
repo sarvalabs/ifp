bf6eedf9df07abc12615d1a829b1297f6d29146f
Rishal SP <rishal@sarva.ai>
2026-02-21 14:08:17 +0530

    propose logic - either repropose or extend

--- fv-veil/IFPSafety/IFP.lean
+++ fv-veil/IFPSafety/IFP.lean
@@ -400,6 +400,9 @@ action respond_propose (n : node) (v : view) (p1 p2 : participant) (c1 c2 : node
       )
     )
   )
+  -- valid proposal pattern: repropose or extend (otherwise should be nil)
+  require (s_max_1 = true ∧ s_max_2 = true ∧ ixn_max_1 = ixn_max_2)
+        ∨ (s_max_1 = false ∧ s_max_2 = false ∧ ixn_max_1 = ixn_max_2)
   -- reproposed if both are locks for same ixn
   require (s_max_1 = true ∧ s_max_2 = true ∧ ixn_max_1 = ixn_max_2) → ixn_max_1 = ixn
   -- extended if both are commits and are same ixn
--- fv-veil/IFPSafety/IFPInvGen.lean
+++ fv-veil/IFPSafety/IFPInvGen.lean
@@ -348,6 +348,9 @@ action respond_propose (n : node) (v : view) (p1 p2 : participant) (c1 c2 : node
       )
     )
   )
+  -- valid proposal pattern: repropose or extend (otherwise should be nil)
+  require (s_max_1 = true ∧ s_max_2 = true ∧ ixn_max_1 = ixn_max_2)
+        ∨ (s_max_1 = false ∧ s_max_2 = false ∧ ixn_max_1 = ixn_max_2)
   require (s_max_1 = true ∧ s_max_2 = true ∧ ixn_max_1 = ixn_max_2) → ixn_max_1 = ixn
   require (s_max_1 = false ∧ s_max_2 = false ∧ ixn_max_1 = ixn_max_2) →
     (parent ixn_max_1 ixn
